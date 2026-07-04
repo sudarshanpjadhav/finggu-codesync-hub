7ec14e7ede3744c93b3ec74030578dee2304d894
Sudarshan P Jadhav <42689334+sudarshanpjadhav@users.noreply.github.com>
2026-07-04 19:38:59 +0530

    finggu-asf: add/update src/FingguRollbackService.js

---
+++ src/FingguRollbackService.js
@@ -0,0 +1,6 @@
+const finggu_rollback_code = async (version) => {
+    // Logic for rolling back code to a previous version
+    return { success: true, message: 'Rollback successful', version };
+};
+
+module.exports = { finggu_rollback_code };
